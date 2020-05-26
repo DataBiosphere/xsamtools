3641fe7e42f52a43e6960377786b117c98d8ba20
Brian Hannafious <brian@connectedsignals.com>
2020-05-26 15:53:11 -0700

    v0.1.0

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ class Install(install.install):
 
 setup(
     name='xsamtools',
-    version='0.0.3',
+    version='0.1.0',
     description='Lightly modified versions of htslib and bcftools.',
     url='https://github.com/xbrianh/xsamtools.git',
     author='Brian Hannafious',

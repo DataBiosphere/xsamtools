b23fb9d1b4c8240544fa2f916a692a131603d33d
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2020-05-28 12:53:15 -0600

    Add subsampling to VCF streams (#15)

--- xsamtools/vcf.py
+++ xsamtools/vcf.py
@@ -1,6 +1,8 @@
 import io
+import os
 from uuid import uuid4
 from multiprocessing import cpu_count
+from tempfile import NamedTemporaryFile
 import subprocess
 
 from terra_notebook_utils import xprofile
@@ -21,15 +23,36 @@ def _merge(input_filepaths, output_filepath):
                    + [fp for fp in input_filepaths])
 
 
+def _view(input_filepath, output_filepath, samples):
+    with NamedTemporaryFile() as tf:
+        with open(tf.name, "w") as fh:
+            fh.write(os.linesep.join(samples))
+        subprocess.run([samtools.paths['bcftools'],
+                        "view",
+                        "-o", output_filepath,
+                        "-O", "z",
+                        "-S", tf.name,
+                        "--threads", f"{2 * cores_available}",
+                        input_filepath])
+
+
 @xprofile.profile("combine")
 def combine(src_bucket_name, src_keys, dst_bucket_name, dst_key):
     readers = [pipes.BlobReaderProcess(src_bucket_name, key) for key in src_keys]
     writer = pipes.BlobWriterProcess(dst_bucket_name, dst_key)
     try:
         _merge([r.filepath for r in readers], writer.filepath)
-    except Exception:
-        raise
     finally:
         for reader in readers:
             reader.close()
+        writer.close()
+
+@xprofile.profile("subsample")
+def subsample(src_bucket_name, src_key, dst_bucket_name, dst_key, samples):
+    reader = pipes.BlobReaderProcess(src_bucket_name, src_key)
+    writer = pipes.BlobWriterProcess(dst_bucket_name, dst_key)
+    try:
+        _view(reader.filepath, writer.filepath, samples)
+    finally:
+        reader.close()
         writer.close()

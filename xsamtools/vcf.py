541c6d0cc3ace978f155fd2abdab2ec2512efc7e
Brian Hannafious <brian@connectedsignals.com>
2020-05-28 17:18:20 -0700

    Make subsample work with local or gs:// paths

--- xsamtools/vcf.py
+++ xsamtools/vcf.py
@@ -48,11 +48,29 @@ def combine(src_bucket_name, src_keys, dst_bucket_name, dst_key):
         writer.close()
 
 @xprofile.profile("subsample")
-def subsample(src_bucket_name, src_key, dst_bucket_name, dst_key, samples):
-    reader = pipes.BlobReaderProcess(src_bucket_name, src_key)
-    writer = pipes.BlobWriterProcess(dst_bucket_name, dst_key)
+def subsample(src_path: str, dst_path: str, samples):
+    reader = _get_reader(src_path)
+    writer = _get_writer(dst_path)
     try:
         _view(reader.filepath, writer.filepath, samples)
     finally:
         reader.close()
         writer.close()
+
+def _get_reader(path):
+    if path.startswith("gs://"):
+        bucket, key = path[5:].split("/", 1)
+        return pipes.BlobReaderProcess(bucket, key)
+    else:
+        fh = open(path)
+        fh.filepath = path
+        return fh
+
+def _get_writer(path):
+    if path.startswith("gs://"):
+        bucket, key = path[5:].split("/", 1)
+        return pipes.BlobWriterProcess(bucket, key)
+    else:
+        fh = open(path, "wb")
+        fh.filepath = path
+        return fh

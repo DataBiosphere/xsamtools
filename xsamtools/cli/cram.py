b773a09ec4ac6298c58fd789fc76de485d1abe54
DailyDreaming <lblauvel@ucsc.edu>
2020-08-11 15:28:09 -0400

    Add Boilerplate.

---
+++ xsamtools/cli/cram.py
@@ -0,0 +1,44 @@
+"""
+CRAM file utilities
+"""
+import argparse
+import datetime
+
+from xsamtools import cram
+from xsamtools.cli import dispatch
+
+
+cram_cli = dispatch.group("cram")
+
+
+@cram_cli.command("view", arguments={
+    "--cram": dict(type=str,
+                   required=True,
+                   help="Input cram file. This can be a Google Storage object if prefixed with 'gs://'."),
+    "--crai": dict(type=str,
+                   required=False,
+                   help="Input crai file. This can be a Google Storage object if prefixed with 'gs://'.  "
+                        "If not specified, one will be generated for you (this may take a long time)."),
+    # TODO: add an argument to intake a BED file, as that's the more rational use-case
+    "--regions": dict(type=str,
+                      required=False,
+                      default=None,
+                      help="A comma-delimited list of regions of sequence in the input cram file to subset as the "
+                           "output CRAM.  If unspecified, the entire file will be copied over."),
+    "-C": dict(action='store_true',
+               required=False,
+               help="Write the output file in CRAM format."),
+    "--output": dict(type=str,
+                     required=False,
+                     help="Output file. This can be a Google Storage object if prefixed with 'gs://'.  "
+                          "If unspecified, this will be created wherever the input cram file was located.")
+})
+def view(args: argparse.Namespace):
+    """
+    A limited wrapper around "samtools view", but with functions to operate on google cloud bucket keys.
+    """
+    if not args.output:
+        time_stamp = str(datetime.datetime.now()).split('.')[0].replace(':', '').replace(' ', '-')
+        args.output = f'{args.input}.{time_stamp}.output.cram'  # schema output, i.e. gs:// or file//:, is preserved
+    # cram.view(cram=args.cram, crai=args.crai, regions=args.regions, output=args.output)
+    print(args.cram, args.crai, args.C, args.regions, args.output)
---
+++ xsamtools/cram.py
@@ -0,0 +1,88 @@
+import io
+import os
+from uuid import uuid4
+from multiprocessing import cpu_count
+from tempfile import NamedTemporaryFile
+import subprocess
+
+from terra_notebook_utils import xprofile, drs
+
+from xsamtools import pipes, vcf, samtools
+
+
+cores_available = cpu_count()
+
+
+def _merge(input_filepaths, output_filepath):
+    subprocess.run([samtools.paths['bcftools'],
+                    "merge",
+                    "--no-index",
+                    "-o", output_filepath,
+                    "-O", "z",
+                    "--threads", f"{2 * cores_available}"]
+                   + [fp for fp in input_filepaths])
+
+
+def _view(input_filepath, output_filepath, samples):
+    with NamedTemporaryFile() as tf:
+        with open(tf.name, "w") as fh:
+            fh.write(os.linesep.join(samples))
+        subprocess.run([samtools.paths['bcftools'],
+                        "view",
+                        "-o", output_filepath,
+                        "-O", "z",
+                        "-S", tf.name,
+                        "--threads", f"{2 * cores_available}",
+                        input_filepath])
+
+
+def _stats(input_filepath):
+    subprocess.run([samtools.paths['bcftools'],
+                    "stats",
+                    "--threads", f"{2 * cores_available}",
+                    input_filepath])
+
+@xprofile.profile("combine")
+def combine(src_files, output_file):
+    readers = [_get_reader(fp) for fp in src_files]
+    writer = _get_writer(output_file)
+    try:
+        _merge([r.filepath for r in readers], writer.filepath)
+    finally:
+        for reader in readers:
+            reader.close()
+        writer.close()
+
+@xprofile.profile("subsample")
+def subsample(src_path: str, dst_path: str, samples):
+    reader = _get_reader(src_path)
+    writer = _get_writer(dst_path)
+    try:
+        _view(reader.filepath, writer.filepath, samples)
+    finally:
+        reader.close()
+        writer.close()
+
+def stats(src_path):
+    reader = _get_reader(src_path)
+    try:
+        _stats(reader.filepath)
+    finally:
+        reader.close()
+
+def _get_reader(path):
+    if path.startswith("gs://") or path.startswith("drs://"):
+        return pipes.BlobReaderProcess(path)
+    else:
+        fh = open(path)
+        fh.filepath = path
+        return fh
+
+def _get_writer(path):
+    if path.startswith("gs://"):
+        bucket, key = path[5:].split("/", 1)
+        return pipes.BlobWriterProcess(bucket, key)
+    else:
+        fh = open(path, "wb")
+        fh.filepath = path
+        return fh

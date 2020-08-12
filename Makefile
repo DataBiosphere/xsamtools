5434eeb845ae893b53de27e80b5317e79677e086
DailyDreaming <lblauvel@ucsc.edu>
2020-08-11 22:13:24 -0400

    Install samtools binary.
    Update samtools install.
    
    Address review comments.
    
    Address review comments.
    
    Address review comments.
    
    Remove version cruft.
    
    Make samtools version explicit.
    
    Path mapping.

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 include common.mk
 
 MODULES=xsamtools tests
+SAMTOOLS_VERSION=1.10
 
 export TNU_TESTMODE?=workspace_access
 
@@ -16,7 +17,13 @@ tests:
 	PYTHONWARNINGS=ignore:ResourceWarning coverage run --source=xsamtools \
 		-m unittest discover --start-directory tests --top-level-directory . --verbose
 
-package_samtools: clean_samtools htslib.tar.bz2 bcftools.tar.bz2
+package_samtools: clean_samtools samtools.tar.bz2 htslib.tar.bz2 bcftools.tar.bz2
+
+samtools.tar.bz2:
+	git clone --depth 1 -b $(SAMTOOLS_VERSION) https://github.com/samtools/samtools.git
+	(cd samtools ; rm -rf .git ; autoheader && autoconf)
+	tar cjf samtools.tar.bz2 samtools
+	rm -rf samtools
 
 htslib.tar.bz2:
 	git clone --depth 1 -b xbrianh-readers-idx https://github.com/xbrianh/htslib
@@ -31,7 +38,7 @@ bcftools.tar.bz2:
 	rm -rf bcftools
 
 clean_samtools:
-	rm -rf bcftools bcftools.tar.bz2 htslib htslib.tar.bz2
+	rm -rf samtools samtools.tar.bz2 bcftools bcftools.tar.bz2 htslib htslib.tar.bz2
 
 version: xsamtools/version.py
 
@@ -59,10 +66,12 @@ image-force:
 publish-image: image
 	docker push $(XSAMTOOLS_IMAGE_NAME)
 
-test-samtools: build/htslib/htsfile build/bcftools/bcftools
+test-samtools: build/samtools/samtools build/htslib/htsfile build/bcftools/bcftools
 build/htslib/htsfile:
 	python setup.py bdist_wheel
 build/bcftools/bcftools:
+	python setup.py bdist_wheel
+build/samtools/samtools:
 	python setup.py bdist_wheel
 
 .PHONY: test lint mypy tests clean sdist build install package_samtools image image-force publish-image
--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import os
 import glob
-import tempfile
 import warnings
 import subprocess
 import traceback
@@ -16,7 +15,6 @@ def _run(cmd: list, **kwargs):
     p.check_returncode()
     return p
 
-
 class BuildPy(build_py.build_py):
     def run(self):
         super().run()
@@ -24,14 +22,16 @@ class BuildPy(build_py.build_py):
             try:
                 _run(["tar", "xjf", "htslib.tar.bz2", "-C", "build"])
                 _run(["tar", "xjf", "bcftools.tar.bz2", "-C", "build"])
+                _run(["tar", "xjf", "samtools.tar.bz2", "-C", "build"])
                 _run(["./configure"], cwd="build/htslib")
+                _run(["./configure"], cwd="build/samtools")
                 _run(["make"], cwd="build/htslib")
                 _run(["make"], cwd="build/bcftools")
+                _run(["make"], cwd="build/samtools")
             except subprocess.CalledProcessError:
-                print("Failed to build htslib/bcftools:")
+                print("Failed to build samtools/htslib/bcftools:")
                 traceback.print_exc()
                 raise
-
 
 class Install(install.install):
     def run(self):
@@ -91,7 +91,7 @@ def get_version():
 setup(
     name='xsamtools',
     version=get_version(),
-    description='Lightly modified versions of htslib and bcftools.',
+    description='Lightly modified versions of samtools, htslib, and bcftools.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/xbrianh/xsamtools.git',

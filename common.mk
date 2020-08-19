352239347fa11a8d88c46c51565ac3a8e8b7a1ff
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2020-08-19 11:50:29 -0600

    Attach package version to dockerfile tag (#57)

--- common.mk
+++ common.mk
@@ -38,12 +38,14 @@ pypi_release:
 	twine upload dist/*
 
 image:
-	docker build -f $(XSAMTOOLS_HOME)/Dockerfile --build-arg XSAMTOOLS_DOCKER_USER --build-arg XSAMTOOLS_HOME -t $(XSAMTOOLS_IMAGE_BASE_NAME) .
+	$(eval export IMAGE_NAME=$(shell $(XSAMTOOLS_HOME)/docker_scripts/image_tag.sh))
+	docker build -f $(XSAMTOOLS_HOME)/Dockerfile --build-arg XSAMTOOLS_DOCKER_USER --build-arg XSAMTOOLS_HOME -t $(IMAGE_NAME) .
 
 image-force:
-	docker build --no-cache -f $(XSAMTOOLS_HOME)/Dockerfile --build-arg XSAMTOOLS_DOCKER_USER --build-arg XSAMTOOLS_HOME -t $(XSAMTOOLS_IMAGE_BASE_NAME) .
+	$(eval export IMAGE_NAME=$(shell $(XSAMTOOLS_HOME)/docker_scripts/image_tag.sh))
+	docker build --no-cache -f $(XSAMTOOLS_HOME)/Dockerfile --build-arg XSAMTOOLS_DOCKER_USER --build-arg XSAMTOOLS_HOME -t $(IMAGE_NAME) .
 
 publish-image: image
-	docker push $(XSAMTOOLS_IMAGE_BASE_NAME)
+	docker push $(IMAGE_NAME)
 
 .PHONY: release image image-force publish-image

db0d55cc0365a507ddee21fffceaffc8f75d8e09
Brian Hannafious <bhannafi@ucsc.edu>
2020-08-19 10:56:25 -0700

    Fix docker image tagging, rename env var

--- docker_scripts/image_tag.sh
+++ docker_scripts/image_tag.sh
@@ -2,4 +2,4 @@
 # This script outputs the docker image tag
 set -euo pipefail
 VERSION=$(git describe --tags --match "v*.*.*" | cut -d '-' -f1)
-echo "${XSAMTOOLS_IMAGE_BASE_NAME}-${VERSION}"
+echo "${XSAMTOOLS_DOCKER_REPO}:${VERSION}"

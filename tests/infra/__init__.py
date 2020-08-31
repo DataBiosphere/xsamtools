987a4204599f4d3f72576e2c8dc93635df520c6b
Brian Hannafious <bhannafi@ucsc.edu>
2020-08-31 13:36:04 -0700

    Reorganize test warning suppression

---
+++ tests/infra/__init__.py
@@ -0,0 +1,11 @@
+import os
+import warnings
+
+
+class SuppressWarningsMixin:
+    def setUp(self):
+        # Suppress the annoying google gcloud _CLOUD_SDK_CREDENTIALS_WARNING warnings
+        warnings.filterwarnings("ignore", "Your application has authenticated using end user credentials")
+        # Suppress unclosed socket warnings
+        warnings.simplefilter("ignore", ResourceWarning)
+        warnings.simplefilter("ignore", UserWarning)

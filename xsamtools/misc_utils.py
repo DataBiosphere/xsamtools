72b9de5bb6cc3f48c2d0199628ba438a0f5a79b9
DailyDreaming <lblauvel@ucsc.edu>
2020-12-01 18:08:51 -0800

    Update error parsing with tests.

--- xsamtools/misc_utils.py
+++ xsamtools/misc_utils.py
@@ -12,16 +12,6 @@ def subprocess_w_stderr_run(cmd: Any, stdout: int = subprocess.PIPE, check: bool
     return process
 
 class SubprocessErrorStdError(subprocess.CalledProcessError):
-    """
-    CalledProcessError that also prints stderr.
-
-    EXAMPLE:
-        Traceback (most recent call last):
-          File "/home/quokka/git/xsamtools/scrap.py", line 37, in <module>
-            raise SubprocessErrorIncludeErrorMessages(p.returncode, cmd, p.stdout, p.stderr)
-        __main__.SubprocessErrorIncludeErrorMessages: Command 'samtools view -C /home/ubuntu/xsamtools/test-cram-slicing/NWD938777.b38.irc.v1.cram -X /home/ubuntu/xsamtools/test-cram-slicing/NWD938777.b38.irc.v1.cram.crai chr1 > /home/ubuntu/xsamtools/2020-11-17-062709.output.cram' returned non-zero exit status 2.
-
-        ERROR: b'/bin/sh: 1: cannot create /home/ubuntu/xsamtools/2020-11-17-062709.output.cram: Directory nonexistent\n'
-    """
+    """CalledProcessError that also prints stderr."""
     def __str__(self):
         return f"{super().__str__()}\n\n{self.stderr.decode('utf-8', errors='replace')}"

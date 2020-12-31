c05e685f016ab51d7b8e5b45e04c5ec6159752c9
DailyDreaming <lblauvel@ucsc.edu>
2020-12-31 01:03:16 -0800

    Review comments.

--- xsamtools/utils.py
+++ xsamtools/utils.py
@@ -7,16 +7,14 @@ def run(cmd: Any, check: bool = True, **kwargs) -> subprocess.CompletedProcess:
     """
     Subprocess.run() that will default to printing stderr when raising on a non-zero error code.
 
-    We cannot use run here as stdout and stderr arguments may not be used with capture_output.
-
-    Stderr will not be printed and error messages will not be seen if capture_output is not True.
+    Stderr will not be printed and propagated error messages will not be seen if capture_output is not True or
+    stderr set to subprocess.PIPE.
     """
-    # stderr must be subprocess.PIPE in order to print stderr currently
     kwargs['check'] = False  # wait and check manually to ensure errors in stderr propagate up
     process = subprocess.run(cmd, **kwargs)
     if process.returncode and check:
         # Error will include the actual error message rather than simply "returned 127".
-        if kwargs['stderr'] == subprocess.PIPE or kwargs.get('capture_output', False):
+        if kwargs.get('stderr', False) == subprocess.PIPE or kwargs.get('capture_output', False):
             raise XSamtoolsCalledProcessError(f'Command: "{cmd}" failed with return code: {process.returncode}'
                                               f'\n\n{process.stderr}')
         raise subprocess.CalledProcessError(process.returncode, cmd, process.stdout, process.stderr)

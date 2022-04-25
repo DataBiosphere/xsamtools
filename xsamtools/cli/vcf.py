d01876a2167f4cbe1d31e187ecacec3edbee7dfa
jesse <brennan@ucsc.edu>
2022-04-25 15:48:27 -0700

    Remove dependency on CLI builder

--- xsamtools/cli/vcf.py
+++ xsamtools/cli/vcf.py
@@ -4,24 +4,8 @@ VCF file utilities
 import argparse
 
 from xsamtools import vcf
-from xsamtools.cli import dispatch
 
 
-vcf_cli = dispatch.group("vcf")
-
-
-@vcf_cli.command("merge", arguments={
-    "--force-samples": dict(action="store_true",
-                            help="resolve duplicate sample names"),
-    "--print-header": dict(action="store_true",
-                           help="print only the merged header and exit"),
-    "--inputs": dict(type=str,
-                     required=True,
-                     help="Input VCFs. These can be Google Storage objects if prefixed with 'gs://'"),
-    "--output": dict(type=str,
-                     required=True,
-                     help="Output VCF. This can be a Google Storage object if prefixed with 'gs://'"),
-})
 def merge(args: argparse.Namespace):
     """
     Merge VCFs stored in google buckets pointed to by `input_keys`.
@@ -39,26 +23,14 @@ def merge(args: argparse.Namespace):
         extra_args.append('--print-header')
     vcf.combine(inputs, args.output, *extra_args)
 
-@vcf_cli.command("subsample", arguments={
-    "--input": dict(type=str,
-                    required=True,
-                    help="Input file. This can be a Google Storage object if prefixed with 'gs://'"),
-    "--output": dict(type=str,
-                     required=True,
-                     help="Output VCF. This can be a Google Storage object if prefixed with 'gs://'"),
-    "--samples": dict(type=str, required=True, help="Comma sepearted list of samples")
-})
+
 def subsample(args: argparse.Namespace):
     """
     Subsample VCF a stored locally or in google bucket.
     """
     vcf.subsample(args.input, args.output, args.samples.split(","))
 
-@vcf_cli.command("stats", arguments={
-    "--input": dict(type=str,
-                    required=True,
-                    help="Input file. This can be a Google Storage object if prefixed with 'gs://'"),
-})
+
 def stats(args: argparse.Namespace):
     """
     Statistics for VCF stored locally or in google bucket.

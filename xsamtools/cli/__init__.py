830a0211dff71efb9f8fda2976e87fb97b490818
jesse <brennan@ucsc.edu>
2022-05-20 11:52:38 -0700

    Fix formatting for flake8

--- xsamtools/cli/__init__.py
+++ xsamtools/cli/__init__.py
@@ -13,15 +13,17 @@ def add_cram_subparser(subparsers):
     cram_subparsers = cram_parser.add_subparsers()
     view_parser = cram_subparsers.add_parser('view', description='A limited wrapper around "samtools view", but with '
                                                                  'functions to operate on google cloud bucket keys.')
-    view_parser.add_argument( "--cram", type=str, required=True,
-                              help="Input cram file. This can be a Google Storage object if prefixed with 'gs://'.")
+    view_parser.add_argument("--cram", type=str, required=True,
+                             help="Input cram file. This can be a Google Storage object if prefixed with 'gs://'.")
     view_parser.add_argument("--crai", type=str, required=False,
-                             help="Input crai file. This can be a Google Storage file (e.g. gs://bucket/key) or a local file.  "
-                                  "If not specified, one will be generated for you (this may take a long time).")
+                             help="Input crai file. This can be a Google Storage file (e.g. gs://bucket/key) or a "
+                                  "local file. If not specified, one will be generated for you (this may take a long "
+                                  "time).")
     # TODO: add an argument to intake a BED file.
     view_parser.add_argument("--regions", type=str, required=False, default=None,
-                             help="A comma-delimited list of regions of sequence in the input cram file to subset as the "
-                                  "output CRAM.  For example, something like: 'ch1,ch2' or 'chromsome_1:10000,chromosome2'.")
+                             help="A comma-delimited list of regions of sequence in the input cram file to subset as "
+                                  "the output CRAM.  For example, something like: 'ch1,ch2' or "
+                                  "'chromsome_1:10000,chromosome2'.")
     view_parser.add_argument("-C", action='store_true', required=False,
                              help="Write the output file in CRAM format.")
     # TODO: Allow this to be a google key.
@@ -41,15 +43,17 @@ def merge_options():
 def add_vcf_subparser(subparsers):
     vcf_parser = subparsers.add_parser('vcf')
     vcf_subparsers = vcf_parser.add_subparsers()
-    merge_parser = vcf_subparsers.add_parser('merge',
-                                             description=dedent("""
+    description = dedent("""
         Merge VCFs stored in google buckets pointed to by `input_keys`.
         Output to `output_key` in the same bucket. Additional arguments will be passed
         on to bcftools merge.
 
         xsamtools vcf merge --bucket "fc-9169fcd1-92ce-4d60-9d2d-d19fd326ff10" \\
         --inputs "a.vcf.gz,b.vcf.gz" \\
-        --output "combined.vcf.gz""").strip(), formatter_class=argparse.RawDescriptionHelpFormatter,
+        --output "combined.vcf.gz""").strip()
+    merge_parser = vcf_subparsers.add_parser('merge',
+                                             description=description,
+                                             formatter_class=argparse.RawDescriptionHelpFormatter,
                                              epilog=merge_options())
     merge_parser.add_argument("--inputs", type=str, required=True,
                               help="Input VCFs. These can be Google Storage objects if prefixed with 'gs://'")
